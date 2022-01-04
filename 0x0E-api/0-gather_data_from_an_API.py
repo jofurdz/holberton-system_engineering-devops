0552043d7f02da90177f94c4ae03558b049bede7
jofurdz <jofurdz@gmail.com>
2022-01-04 11:16:25 -0600

    task 0

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" returns information about employee TO DO list progress"""
+
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+
+    userID = argv[1]
+    userInfo = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.
+        format(userID)).json()
+    name = userInfo.get('name')
+    taskCount = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos?completed=true'.
+        format(userID)).json()
+    taskList = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.
+        format(userID)).json()
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+           name, len(taskCount), len(taskList)))
+    for item in taskCount:
+        print('/t {}'.format(item.get('title')))
